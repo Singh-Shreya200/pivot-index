1602adf7e1fb9ebdbddabc1fb377d10928a4c4a6
Singh-Shreya200 <102677765+Singh-Shreya200@users.noreply.github.com>
2022-04-01 22:13:44 +0530

    Add files via upload

---
+++ pivot index.py
@@ -0,0 +1,10 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        total_sum=sum(nums)
+        left_sum=0
+        for i,num in enumerate(nums):
+            total_sum-=num
+            if left_sum==total_sum:
+                return i
+            left_sum+=num
+        return(-1)
